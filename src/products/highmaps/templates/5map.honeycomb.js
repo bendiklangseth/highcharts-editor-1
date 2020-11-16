787c01e7717f8042d0f02dad12c2e301f384dd6b
bendiklangseth <bendik.langseth@makingwaves.com>
2020-11-16 14:48:36 +0100

    Remove Map option

--- src/products/highmaps/templates/5map.honeycomb.js
+++ src/products/highmaps/templates/5map.honeycomb.js
@@ -25,7 +25,7 @@ SOFTWARE OR THE USE OR OTHER DEALINGS IN THE SOFTWARE.
 
 */
 
-highed.templates.add('Maps', {
+highed.templates.add('Map', {
   title: 'Honeycomb',
   description: [
     'Tilemaps are maps where each area is represented by tiles of equal shape. You can choose from circle or honeycomb which uses hexagonal tiles.',
